3c9b71434f366da6d08a60f21aaabcffa8843b25
Kovács Barna <barni02kovacs@gmail.com>
2023-01-09 00:08:49 +0100

    oldalkeresos modszer implmentalva

--- main.cpp
+++ main.cpp
@@ -52,81 +52,229 @@ int main()
 
     std::vector<Cube> Cubes;
 
-    std::vector<Rectangle> used;
     std::vector<Rectangle> potential = rec_vec;
     bool cubedone = false;
+    Cube tempcube;
 
+    if(rec_vec.size() > 5){
     while(!cubedone)
     {
-        Rectangle temprec1;
-        Rectangle temprec2;
-        Rectangle temprec3;
-        Rectangle temprec4;
-        Rectangle temprec5;
-        Rectangle temprec6 ;
+        Rectangle temprecbase;
+        Rectangle temprecnear;
+        Rectangle temprecfar;
+        Rectangle temprecright;
+        Rectangle temprecleft;
+        Rectangle temprectop;
 
         for(auto i = potential.begin(); i != potential.end();i++)
         {
-            temprec1 = *i;
+            temprecbase = *i;
             potential.erase(i);
 
-            for(Rectangle r : potential)
-            {
-                bool match = false;
-                while(!match)
-                {
-                    int j = 0;
-                    if(sarokeq(temprec1, r, 0, 0))
-                    {
-                        if(sarokeq(temprec1, r, 1, 1))
-                        {
-                            temprec3 = r;
-                            match = true;
-                        }
-                        else if(sarokeq(temprec1, r, 3, 3))
-                        {
-                            temprec6 = r;
-                            match = true;
-                        }
-                    }
-                    else if(sarokeq(temprec1, r, 1, 1))
-                    {
-                        if(sarokeq(temprec1, r, 2, 2))
-                        {
-                            temprec5 = r;
-                            match = true;
-                        }
-                    }
-                    else if(sarokeq(temprec1, r, 2, 2))
-                    {
-                        if(sarokeq(temprec1, r, 3, 3))
-                        {
-                            temprec4 = r;
-                            match = true;
-                        }
-                    }
-                    else if(j < 3)
-                    {
-                        j++;
-                        temprec1.forgat();
-                    }
-                    else
-                    {
-                        match = true;
-                    }
+            bool goodbase = true;
+            bool match = false;
+            int j = 0;
+            int k = 0;
 
+            while(!match) //kozeli(near) oldal kereses
+            {
+                if(sarokeq(temprecbase, potential[j], 0, 0)  && sarokeq(temprecbase, potential[j], 1, 1))
+                {
+                    match = true;
+                    temprecnear = potential[j];
+                    potential.erase(potential.begin() + j);
                 }
+                else if(k < 3)
+                {
+                    potential[j].forgat();
+                    k++;
+                }
+                else if(j < potential.size())
+                {
+                    j++;
+                }
+                else
+                {
+                    match = true;
+                    goodbase = false;
+                    potential.push_back(temprecbase);
+                }
+            }
+
+            match = false;
+            j = 0;
+            k = 0;
+
+            while(!match && goodbase) //tavoli(far) oldal kereses
+            {
+                if(sarokeq(temprecbase, potential[j], 2, 2)  && sarokeq(temprecbase, potential[j], 3, 3))
+                {
+                    match = true;
+                    temprecfar = potential[j];
+                    potential.erase(potential.begin() + j);
+                }
+                else if(k < 3)
+                {
+                    potential[j].forgat();
+                    k++;
+                }
+                else if(j < potential.size())
+                {
+                    j++;
+                }
+                else
+                {
+                    match = true;
+                    goodbase = false;
+                    potential.push_back(temprecbase);
+                    potential.push_back(temprecnear);
+                }
+            }
+
+            match = false;
+            j = 0;
+            k = 0;
+
+            while(!match && goodbase) //jobb(right) oldal kereses
+            {
+                if(sarokeq(temprecbase, potential[j], 1, 1))
+                {
+                    if(sarokeq(temprecbase, potential[j], 2, 2))
+                    {
+                        if(sarokeq(temprecnear, potential[j], 2, 0))
+                        {
+                            if(sarokeq(temprecfar, potential[j], 1, 3))
+                            {
+                                match = true;
+                                temprecright = potential[j];
+                                potential.erase(potential.begin() + j);
+                            }
+                        }
+                    }
+                }
+                else if(k < 3)
+                {
+                    potential[j].forgat();
+                    k++;
+                }
+                else if(j < potential.size())
+                {
+                    j++;
+                }
+                else
+                {
+                    match = true;
+                    goodbase = false;
+                    potential.push_back(temprecbase);
+                    potential.push_back(temprecnear);
+                    potential.push_back(temprecfar);
+                }
+            }
+
+            match = false;
+            j = 0;
+            k = 0;
+
+            while(!match && goodbase) //bal(left) oldal kereses
+            {
+                if(sarokeq(temprecbase, potential[j], 0, 0))
+                {
+                    if(sarokeq(temprecbase, potential[j], 3, 3))
+                    {
+                        if(sarokeq(temprecnear, potential[j], 3, 1))
+                        {
+                            if(sarokeq(temprecfar, potential[j], 0, 2))
+                            {
+                                match = true;
+                                temprecleft = potential[j];
+                                potential.erase(potential.begin() + j);
+                            }
+                        }
+                    }
+                }
+                else if(k < 3)
+                {
+                    potential[j].forgat();
+                    k++;
+                }
+                else if(j < potential.size())
+                {
+                    j++;
+                }
+                else
+                {
+                    match = true;
+                    goodbase = false;
+                    potential.push_back(temprecbase);
+                    potential.push_back(temprecnear);
+                    potential.push_back(temprecfar);
+                    potential.push_back(temprecright);
+                }
+            }
+
+            match = false;
+            j = 0;
+            k = 0;
+
+            while(!match && goodbase) //felso(top) oldal kereses
+            {
+                if(sarokeq(temprecnear, potential[j], 3, 0))
+                {
+                    if(sarokeq(temprecnear, potential[j], 2, 1))
+                    {
+                        if(sarokeq(temprecfar, potential[j], 1, 2))
+                        {
+                            if(sarokeq(temprecfar, potential[j], 0, 3))
+                            {
+                                match = true;
+                                temprectop = potential[j];
+                                potential.erase(potential.begin() + j);
+                            }
+                        }
+                    }
+                }
+                else if(k < 3)
+                {
+                    potential[j].forgat();
+                    k++;
+                }
+                else if(j < potential.size())
+                {
+                    j++;
+                }
+                else
+                {
+                    match = true;
+                    goodbase = false;
+                    potential.push_back(temprecbase);
+                    potential.push_back(temprecnear);
+                    potential.push_back(temprecfar);
+                    potential.push_back(temprecright);
+                    potential.push_back(temprecleft);
+                }
+            }
+
+            if(goodbase)
+            {
+                tempcube.base = temprecbase;
+                tempcube.near = temprecnear;
+                tempcube.far = temprecfar;
+                tempcube.right = temprecright;
+                tempcube.left = temprecleft;
+                tempcube.top = temprectop;
+
+                Cubes.push_back(tempcube);
             }
         }
 
+        cubedone = true;
 
-
-        if(potential.size() == 0 || used.size() == 6) {cubedone = true;}
+    }
     }
 
 
 
-    if(potential.size() == 0) {std::cout << "Nem lehet egy kockat se csinalni." << std::endl;}
+    if(Cubes.size() == 0) {std::cout << "Nem lehet egy kockat se csinalni." << std::endl;}
 
     return 0;
 }
