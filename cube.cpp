438c56d038fe5c1359f8ba822fdfdda2798afe8b
Kovács Barna <barni02kovacs@gmail.com>
2023-01-08 17:29:24 +0100

    'dolgok'

--- cube.cpp
+++ cube.cpp
@@ -1,6 +1,18 @@
 #include "cube.hpp"
 
-Cube::Cube()
+/*Cube::Cube()
 {
 
+}*/
+
+void Cube::cube_build(std::vector<Rectangle> recs)
+{
+    Rectangle side1 = recs[0];
+    std::vector<sarok> sarkok1 = side1.sarkok;
+    recs.erase(recs.begin());
+
+    for(Rectangle r : recs)
+    {
+
+    }
 }
--- cube.hpp
+++ cube.hpp
@@ -2,13 +2,22 @@
 #define CUBE_HPP
 
 #include "rectangle.hpp"
+#include <iostream>
 
 // Sort the imported rectangles and make as many cubes as possible from any vector of rectangles; export the cubes as a vector
 
 class Cube
 {
+
 public:
-    Cube();
+
+    std::vector<Rectangle> sides;
+
+    Cube() {}
+
+    void cube_build(std::vector<Rectangle> recs);
+
+    void cube_check();
 };
 
 #endif // CUBE_HPP
